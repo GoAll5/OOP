d42ae072a653df66a521faaf353baea6ecc413a7
Ринат <Rinat89.2001@yandex.ru>
2020-11-18 21:42:13 +0500

    lab3 test and program

---
+++ Tests/Test3.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestFixture]
+    class Test3
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+        //[Test]
+        //public void TestShow()
+        //{
+        //    //arrange 
+            
+
+        //    //act
+           
+            
+
+        //    //assert ожидали - получили
+            
+        //}
+    }
+}
